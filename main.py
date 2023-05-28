b75b46b8b99d676c682a6e0e6e00222014d69498
f-hluchnik <frantahluchnik@gmail.com>
2023-05-28 18:52:55 +0200

    Add Dockerfile, use logging module, update imports.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from APIClient import APIClient
-from DataProcessor import DataProcessor
-from FileWriter import FileWriter
+from dotenv import load_dotenv
+from src.APIClient import APIClient
+from src.DataProcessor import DataProcessor
+from src.FileWriter import FileWriter
+import logging
 
 def main():
-    api_url = "www.example.com"
+    load_dotenv()
+    
+    api_url = "https://www.example.com"
     file_path = "output"
 
     api_client = APIClient(api_url)
@@ -15,7 +19,7 @@ def main():
     file_writer = FileWriter(file_path)
     file_writer.write_to_file(processed_data)
 
-    print("Download and saving was successful.")
+    logging.info("Download and saving was successful.")
 
-if __name__ == 'main':
+if __name__ == '__main__':
     main()
--- src/APIClient.py
+++ src/APIClient.py
@@ -1,10 +1,14 @@
 import requests
+import os
+import logging
 
 class APIClient:
     def __init__(self, api_url) -> None:
         self.api_url = api_url
+        self.api_key = os.environ.get('API_KEY')
     
     def fetch_data(self):
         response = requests.get(self.api_url)
+        logging.info('Data download successful.')
         data = response.json()
         return data
